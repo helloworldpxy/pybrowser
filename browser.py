c1ddf93ef9c7a6a6f46d056419e21a243aaf40d1
RedstoneLu <97652352+RedstoneLu@users.noreply.github.com>
2024-06-21 21:49:08 +0800

    此版本为浏览器的发行版
    优化源代码，将起始页换为本地起始页。

--- browser.py
+++ browser.py
@@ -1,25 +1,27 @@
-# v1.0
+# v1.1
 # created by HelloWorld05
 # in 2024.06.19
+# modified by RedstoneLu
+# in 2024.06.21
+
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtWebEngineWidgets import *
 import sys
-#导入相应模块
 
 class WebView(QWebEngineView):
     def __init__(self, parent):
         super().__init__(parent)
 
     def createWindow(self, webWindowType):
-        return main_demo.browser
+        return main_window.browser
 
-class MainDemo(QMainWindow):
+class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.setWindowTitle('Pybrowser')
-        self.setWindowIcon(QIcon('icons/python.png'))
+        self.setWindowTitle('LCbrowser')
+        self.setWindowIcon(QIcon('icons/icon.png'))
         self.resize(1024, 768)
         self.show()
         # 添加URL地址栏
@@ -35,7 +37,8 @@ class MainDemo(QMainWindow):
         self.tabs.setTabsClosable(True)
         # 设置关闭按钮的槽
         self.tabs.tabCloseRequested.connect(self.close_current_tab)
-        self.add_new_tab(QUrl('https://cn.bing.com/'), 'Bing搜索')
+         # 修改：默认加载本地HTML文件
+        self.add_new_tab(QUrl.fromLocalFile("/startpage/index.html"), label='Local HTML')
         self.setCentralWidget(self.tabs)
         new_tab_action = QAction(QIcon('icons/add_page.png'), 'New Page', self)
         new_tab_action.triggered.connect(self.add_new_tab)
@@ -45,30 +48,28 @@ class MainDemo(QMainWindow):
         navigation_bar.setIconSize(QSize(16, 16))
         self.addToolBar(navigation_bar)
         # 添加前进、后退、停止加载和刷新的按钮
-        back_button = QAction(QIcon('icons/back.png'), 'Back', self)
-        forward_button = QAction(QIcon('icons/forward.png'), 'Forward', self)
-        stop_button = QAction(QIcon('icons/stop.png'), 'Stop', self)
-        reload_button = QAction(QIcon('icons/renew.png'), 'Reload', self)
-
-        back_button.triggered.connect(self.tabs.currentWidget().back)
-        forward_button.triggered.connect(self.tabs.currentWidget().forward)
-        stop_button.triggered.connect(self.tabs.currentWidget().stop)
-        reload_button.triggered.connect(self.tabs.currentWidget().reload)
-
-        # 将按钮添加到导航栏上
-        navigation_bar.addAction(back_button)
-        navigation_bar.addAction(forward_button)
-        navigation_bar.addAction(stop_button)
-        navigation_bar.addAction(reload_button)
-        navigation_bar.addSeparator()
+        self.add_navigation_buttons(navigation_bar)  # 修改：添加了add_navigation_buttons方法
         navigation_bar.addWidget(self.urlbar)
 
+    def add_navigation_buttons(self, navigation_bar):  # 修改：新方法，减少代码重复
+        buttons = [
+            ('back', 'Back', self.tabs.currentWidget().back),
+            ('forward', 'Forward', self.tabs.currentWidget().forward),
+            ('stop', 'Stop', self.tabs.currentWidget().stop),
+            ('reload', 'Reload', self.tabs.currentWidget().reload),
+        ]
+        for icon, text, slot in buttons:
+            action = QAction(QIcon(f'icons/{icon}.png'), text, self)
+            action.triggered.connect(slot)
+            navigation_bar.addAction(action)
 
     # 响应回车按钮，将浏览器当前访问的URL设置为用户输入的URL
     def navigate_to_url(self):
-        current_url = QUrl(self.urlbar.text())
-        if current_url.scheme() == '':
-            current_url.setScheme('http')
+        url_text = self.urlbar.text()
+        # 修改：检查URL是否有效
+        if not url_text.startswith(('http:', 'https:')):
+            url_text = 'http://' + url_text
+        current_url = QUrl(url_text)
         self.tabs.currentWidget().load(current_url)
 
     # 将当前网页的链接更新到地址栏
@@ -79,9 +80,9 @@ class MainDemo(QMainWindow):
         self.urlbar.setText(url.toString())
         self.urlbar.setCursorPosition(0)
 
-    # 添加新的标签页
-    def add_new_tab(self, qurl=QUrl(''), label='Blank'):
-        # 设置浏览器
+         # 添加新的标签页
+    def add_new_tab(self, qurl=QUrl.fromLocalFile("/startpage/index.html"), label='Local HTML'):
+                # 设置浏览器
         self.browser = WebView(self)
         self.browser.load(qurl)
         # 为标签添加索引方便管理
@@ -98,8 +99,9 @@ class MainDemo(QMainWindow):
             self.add_new_tab()
 
     def current_tab_changed(self, i):
-        qurl = self.tabs.currentWidget().url()
-        self.renew_urlbar(qurl, self.tabs.currentWidget())
+        if i != -1:  # 修改：添加了对索引的检查
+            qurl = self.tabs.currentWidget().url()
+            self.renew_urlbar(qurl, self.tabs.currentWidget())
 
     def close_current_tab(self, i):
         # 若当前标签页只有一个则不关闭
@@ -107,9 +109,8 @@ class MainDemo(QMainWindow):
             return
         self.tabs.removeTab(i)
 
-
 if __name__ == '__main__':
-    my_application = QApplication(sys.argv)  # 创建QApplication类的实例
-    main_demo = MainDemo()
-    main_demo.show()
-    my_application.exec_()
+    app = QApplication(sys.argv)  # 修改：将my_application改为更符合PEP 8的app
+    main_window = MainWindow()  # 修改：将main_demo改为更描述性的main_window
+    main_window.show()
+    sys.exit(app.exec_())  # 修改：使用sys.exit来确保程序干净退出
