3408ec2d7a94d99f264a1a92a2522eb6f31b8554
HelloWorld05 <hklpl@icloud.com>
2025-05-20 01:20:17 +0800

    Add files via upload

--- browser.py
+++ browser.py
@@ -1,17 +1,22 @@
-# v1.0
+# v1.5
 # created by HelloWorld05
-# in 2024.06.26
+# in 2025.05.20
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtWebEngineWidgets import *
 import sys
-#导入相应模块
+
+# 导入相应模块
 class WebView(QWebEngineView):
     def __init__(self, parent):
         super().__init__(parent)
+
     def createWindow(self, webWindowType):
-        return main_demo.browser
+        new_browser = WebView(self)
+        main_demo.add_new_tab(qurl=QUrl(), label='New Tab')
+        return new_browser
+
 class MainDemo(QMainWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -32,7 +37,7 @@ class MainDemo(QMainWindow):
         self.tabs.setTabsClosable(True)
         # 设置关闭按钮的槽
         self.tabs.tabCloseRequested.connect(self.close_current_tab)
-        self.add_new_tab(QUrl('https://cn.bing.com/'), 'Bing搜索')
+        self.add_new_tab(qurl=QUrl('https://cn.bing.com/'), label='Bing搜索')
         self.setCentralWidget(self.tabs)
         new_tab_action = QAction(QIcon('icons/add_page.png'), 'New Page', self)
         new_tab_action.triggered.connect(self.add_new_tab)
@@ -46,10 +51,10 @@ class MainDemo(QMainWindow):
         forward_button = QAction(QIcon('icons/forward.png'), 'Forward', self)
         stop_button = QAction(QIcon('icons/stop.png'), 'Stop', self)
         reload_button = QAction(QIcon('icons/renew.png'), 'Reload', self)
-        back_button.triggered.connect(self.tabs.currentWidget().back)
-        forward_button.triggered.connect(self.tabs.currentWidget().forward)
-        stop_button.triggered.connect(self.tabs.currentWidget().stop)
-        reload_button.triggered.connect(self.tabs.currentWidget().reload)
+        back_button.triggered.connect(lambda: self.tabs.currentWidget().back() if self.tabs.currentWidget() else None)
+        forward_button.triggered.connect(lambda: self.tabs.currentWidget().forward() if self.tabs.currentWidget() else None)
+        stop_button.triggered.connect(lambda: self.tabs.currentWidget().stop() if self.tabs.currentWidget() else None)
+        reload_button.triggered.connect(lambda: self.tabs.currentWidget().reload() if self.tabs.currentWidget() else None)
         # 将按钮添加至导航栏上
         navigation_bar.addAction(back_button)
         navigation_bar.addAction(forward_button)
@@ -57,12 +62,14 @@ class MainDemo(QMainWindow):
         navigation_bar.addAction(reload_button)
         navigation_bar.addSeparator()
         navigation_bar.addWidget(self.urlbar)
+
     # 响应回车按钮，将浏览器当前访问的URL设置为用户输入的URL
     def navigate_to_url(self):
         current_url = QUrl(self.urlbar.text())
         if current_url.scheme() == '':
             current_url.setScheme('http')
         self.tabs.currentWidget().load(current_url)
+
     # 将当前网页的链接更新到地址栏
     def renew_urlbar(self, url, browser=None):
         # 非当前窗口不更新URL
@@ -70,8 +77,9 @@ class MainDemo(QMainWindow):
             return
         self.urlbar.setText(url.toString())
         self.urlbar.setCursorPosition(0)
+
     # 添加新的标签页
-    def add_new_tab(self, qurl=QUrl(''), label='Blank'):
+    def add_new_tab(self, qurl=QUrl(), label='Blank'):
         # 设置浏览器
         self.browser = WebView(self)
         self.browser.load(qurl)
@@ -82,18 +90,22 @@ class MainDemo(QMainWindow):
         # 将标签标题改为网页相关的标题
         self.browser.loadFinished.connect(
             lambda _, i=i, browser=self.browser: self.tabs.setTabText(i, self.browser.page().title()))
+
     # 双击标签栏打开新页面
     def tab_open(self, i):
         if i == -1:
             self.add_new_tab()
+
     def current_tab_changed(self, i):
         qurl = self.tabs.currentWidget().url()
         self.renew_urlbar(qurl, self.tabs.currentWidget())
+
     def close_current_tab(self, i):
         # 若当前标签页只有一个则不关闭
         if self.tabs.count() < 2:
             return
         self.tabs.removeTab(i)
+
 if __name__ == '__main__':
     my_application = QApplication(sys.argv)  # 创建QApplication类的实例
     main_demo = MainDemo()
